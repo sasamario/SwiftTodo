066fc7e16e1a04018c71d9dff5f5c70136eae089
sasama <mametaku3@yahoo.co.jp>
2021-01-27 22:15:04 +0900

    Add：TodoデータをRealmに追加する処理を実装

--- SwiftTodo/AddViewController.swift
+++ SwiftTodo/AddViewController.swift
@@ -6,16 +6,35 @@
 //
 
 import UIKit
+import RealmSwift
 
 class AddViewController: UIViewController {
 
+    @IBOutlet weak var textBox: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
+    //TodoをRealmに追加
+    @IBAction func addButton(_ sender: Any) {
+        let realm = try! Realm()
+        
+        //Todoインスタンス化
+        let todo = Todo()
+        todo.title = textBox.text!
+        
+        //realmにTodoデータを追加
+        try! realm.write {
+            realm.add(todo)
+        }
+        
+        //ひとつ前のViewControllerに戻る
+        self.navigationController?.popViewController(animated: true)
+    }
+    
     /*
     // MARK: - Navigation
 
