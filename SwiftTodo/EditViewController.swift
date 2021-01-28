d991c1e02d5f97f1af682e0aeb15a0ae55ceb713
sasama <mametaku3@yahoo.co.jp>
2021-01-28 23:54:54 +0900

    Add：EditViewControllerを追加
    Todoの編集用のコントローラ

---
+++ SwiftTodo/EditViewController.swift
@@ -0,0 +1,31 @@
+//
+//  EditViewController.swift
+//  SwiftTodo
+//
+//  Created by 笹間巧馬 on 2021/01/28.
+//
+
+import UIKit
+import RealmSwift
+
+class EditViewController: UIViewController {
+    
+    @IBOutlet weak var todoText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
